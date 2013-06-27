896f5b345b987eba2dc8afe1dd9cf5857d2bcbf9
Damian Mooyman <damian.mooyman@gmail.com>
2013-06-28 11:06:08 +1200

    API robots.yml updated with new configuration options, removal of obsolete options, and fixed to conform to naming conventions Added documentation Fixed indentation

--- README.md
+++ README.md
@@ -1,4 +1,109 @@
-silverstripe-robots
-===================
+# Robots.txt generation module for Silverstripe
 
-Simple robots generation module for Silverstripe
+This module provides simple robots.txt generation for Silverstripe, with various configuration
+options available.
+
+When a site is not in live mode (such as on a testing domain) it will respectively
+block the entire domain, ensuring that (at least respectful) search engines will
+refrain from indexing your test site.
+
+This doesn't support complex rules (such as multiple rules for different user agents),
+as it's designed to cater to the 99.9% of cases which won't require manual configuration.
+
+## Credits and Authors
+
+ * Damian Mooyman - <https://github.com/tractorcow/silverstripe-robots>
+
+## Requirements
+
+ * SilverStripe 3 or above
+ * PHP 5.3
+
+## Installation Instructions
+
+ * Extract all files into the 'robots' folder under your Silverstripe root, or install using composer
+
+```bash
+composer require "tractorcow/silverstripe-robots": "3.0.*@dev"
+```
+
+ * Make sure you are correctly setting your environment for it to work properly
+ * It's also advisable to either install the googlesitemaps module, or to create a physical `sitemap.xml` in your site root.
+
+## Configuration
+
+Basic robots configuration can be found at [_config/robots.yml](_config/robots.yml)
+
+You can add a page or pattern to be blocked by adding it to the disallowedUrls configuration
+
+```yaml
+---
+Name: myrobotsconfiguration
+After: '#robotsconfiguration'
+---
+Robots:
+  disallowed_urls:
+    - 'mysecretpage.html'
+    - '_private'
+    - 'Documents-and-Settings/Ricky/My-Documents/faxes/sent-faxes'
+```
+
+Also by default, any page with 'ShowInSearch' set to false will also be excluded. This
+can be useful for hiding auxilary pages like "thanks for signing up", or error pages.
+
+You can turn this off (if you really absolutely think you need to) using the below
+
+```yaml
+---
+Name: myrobotsconfiguration
+After: '#robotsconfiguration'
+---
+Robots:
+  disallow_unsearchable: false
+```
+
+By default the module will check for a sitemap file in `/sitemap.xml`, or will assume
+one is there if the googlesitemap module is installed. You can set a custom file location
+using the below configuration.
+
+```yaml
+---
+Name: myrobotsconfiguration
+After: '#robotsconfiguration'
+---
+Robots:
+  sitemap: '/sitemap_index.xml'
+```
+
+## Need more help?
+
+Message or email me at damian.mooyman@gmail.com or, well, read the code!
+
+## License
+
+Copyright (c) 2013, Damian Mooyman
+All rights reserved.
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+ * The name of Damian Mooyman may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
--- code/Robots.php
+++ code/Robots.php
@@ -6,145 +6,116 @@
  * @author Damian Mooyman
  */
 class Robots extends Controller {
-	
+
 	/**
-	 * Enable this module
-	 */
-	public static function enable() {
-		self::set_enabled(true);
-	}
-	
-	/**
-	 * Disable this module
-	 */
-	public static function disable() {
-		self::set_enabled(false);
-	}
-	
-	/**
-	 * Determine if this module is enabled
+	 * Determines if this is a public site
 	 * 
-	 * @return boolean
+	 * @return boolean flag indicating if this robots is for a public site
 	 */
-	public static function get_enabled() {
-		return Config::inst()->get('Robots', 'enabled');
+	protected function isPublic() {
+		return Director::isLive();
 	}
-	
-	/**
-	 * Set this module as enabled or disabled
-	 * 
-	 * @param boolean $enabled
-	 */
-	public static function set_enabled($enabled) {
-		Config::inst()->set('Robots', 'enabled', $enabled);
-	}
-    
-    /**
-     * Determines if this is a public site
-	 * 
-     * @return boolean flag indicating if this robots is for a public site
-     */
-    protected function isPublic()
-    {
-        return Director::isLive();
-    }
 
 	/**
 	 * Generates the response containing the robots.txt content
 	 * 
 	 * @return SS_HTTPResponse
 	 */
-    public function index()
-    {
-        $text = "";
-        $text .= $this->renderSitemap();
-        $text .= "User-agent: *\n";
-        $text .= $this->renderDisallow();
-        $text .= $this->renderAllow();
-        
-        $response = new SS_HTTPResponse($text, 200);
-        $response->addHeader("Content-Type", "text/plain; charset=\"utf-8\"");
-        return $response;
-    }
-    
+	public function index() {
+		$text = "";
+		$text .= $this->renderSitemap();
+		$text .= "User-agent: *\n";
+		$text .= $this->renderDisallow();
+		$text .= $this->renderAllow();
+
+		$response = new SS_HTTPResponse($text, 200);
+		$response->addHeader("Content-Type", "text/plain; charset=\"utf-8\"");
+		return $response;
+	}
+
 	/**
 	 * Renders the sitemap link reference
 	 * 
 	 * @return string
 	 */
-    protected function renderSitemap()
-    {
+	protected function renderSitemap() {
+
 		// No sitemap if not public
-        if(!$this->isPublic()) return '';
-        
+		if (!$this->isPublic()) return '';
+
+		// Check if sitemap is configured
+		$sitemap = Robots::config()->sitemap;
+		if (empty($sitemap)) return '';
+
 		// Skip sitemap if not available
-        if(!class_exists('GoogleSitemap') && !Director::fileExists('/sitemap.xml')) {
-            return '';
+		if (!class_exists('GoogleSitemap') && !Director::fileExists($sitemap)) {
+			return '';
 		}
-        
-        return sprintf("Sitemap: %s\n", Director::absoluteURL('/sitemap.xml'));
-    }
-    
+
+		// Report the sitemap location
+		return sprintf("Sitemap: %s\n", Director::absoluteURL($sitemap));
+	}
+
 	/**
 	 * Renders the list of disallowed pages
 	 * 
 	 * @return string
 	 */
-    protected function renderDisallow()
-    {
-        // List only disallowed urls
-        $text = '';
-        foreach($this->disallowedUrls() as $url) {
-            $text .= sprintf("Disallow: %s\n", $url);
+	protected function renderDisallow() {
+		// List only disallowed urls
+		$text = '';
+		foreach ($this->disallowedUrls() as $url) {
+			$text .= sprintf("Disallow: %s\n", $url);
 		}
-        return $text;
-    }
-    
-    
-    /**
+		return $text;
+	}
+
+	/**
 	 * Renders the list of allowed pages, if any
 	 * 
 	 * @return string
 	 */
-    protected function renderAllow() {
-        $text = '';
-		foreach($this->allowedUrls() as $url) {
-            $text .= sprintf("Allow: %s\n", $url);
+	protected function renderAllow() {
+		$text = '';
+		foreach ($this->allowedUrls() as $url) {
+			$text .= sprintf("Allow: %s\n", $url);
 		}
 		return $text;
-    }
-	
+	}
+
 	/**
 	 * Returns an array of disallowed URLs
 	 * 
 	 * @return array
 	 */
 	protected function disallowedUrls() {
-		
-        // If not public, disallow all
-        if(!$this->isPublic()) return array("/");
-		
+
+		// If not public, disallow all
+		if (!$this->isPublic()) return array("/");
+
 		// Get configured disallowed urls
-		$urls = (array)Config::inst()->get('Robots', 'disallowedUrls');
-		
+		$urls = (array) Robots::config()->disallowed_urls;
+
 		// Add all pages where ShowInSearch is false
-		$unsearchablePages = SiteTree::get()->where('"SiteTree"."ShowInSearch" = 0');
-		foreach($unsearchablePages as $page) {
-			$link = $page->Link();
-			// Don't disallow home page
-			if($link !== '/') $urls[] = $link;
+		if (Robots::config()->disallow_unsearchable) {
+			$unsearchablePages = SiteTree::get()->where('"SiteTree"."ShowInSearch" = 0');
+			foreach ($unsearchablePages as $page) {
+				$link = $page->Link();
+				// Don't disallow home page
+				if ($link !== '/') $urls[] = $link;
+			}
 		}
-		
+
 		return array_unique($urls);
 	}
-	
+
 	/**
 	 * Returns an array of allowed URLs
 	 * 
 	 * @return array
 	 */
 	protected function allowedUrls() {
-		return (array)Config::inst()->get('Robots', 'allowedUrls');
+		return (array) Robots::config()->allowed_urls;
 	}
 
 }
